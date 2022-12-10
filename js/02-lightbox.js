02ebadc44d78e9f29f8ba998f5dd8559de2bdeef
martynakulis <kulismartyna@gmail.com>
2022-12-10 20:16:36 +0100

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__item');
+  galleryLink.setAttribute('href', item.original);
+  gallery.append(galleryLink);
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.alt = item.description;
+  galleryLink.append(image);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
